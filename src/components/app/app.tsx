2f05a05b59f2823564f8c32b06a54364ca4961ef
iivic17 <ilijaivic17@gmail.com>
2022-01-07 15:05:39 +0100

    Cannot use import statement outside a module error fix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,30 @@
 import './app.css';
 import CodeCell from '../code-cell/code-cell';
-import { initialCode } from '../template';
 
 const App: React.FC = () => {
+	const initialCode = `   
+    	import React, { useState } from 'react';
+    	import ReactDOM from 'react-dom';
+
+    	const App = () => {
+        	const [counter, setCounter] = useState(0);
+
+        	const onClick = () => {
+            	setCounter(counter + 1);
+        	};
+
+        	return (
+            	<div>
+                	<h1>Hello there</h1>
+                	<p>This is a React app!</p>
+                	<button onClick={onClick}>Increase me ({counter})</button>
+            	</div>
+       		);
+    	};
+
+   		ReactDOM.render(<App />, document.querySelector('#root'));
+	`;
+
 	return (
 		<div>
 			<CodeCell initialValue={initialCode} />
--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -1,10 +1,29 @@
 import './preview.css';
 import { useRef, useEffect } from 'react';
 import PreviewProps from './preview-props';
-import { rootHtml } from '../template';
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
 	const iframe = useRef<any>();
+
+	const rootHtml = `
+    	<html>
+        	<head></head>
+        	<body>
+            	<div id="root"></div>
+            	<script>
+                    	window.addEventListener('message', (event) => {
+                    	try {
+                        	eval(event.data);
+                    	} catch (err) {
+                        	const root = document.querySelector('#root');
+                        	root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
+                        	throw err;
+                    	}
+                	}, false);
+            	</script>
+        	</body>
+    	</html>
+	`;
 
 	useEffect(() => {
 		iframe.current.srcdoc = rootHtml;
@@ -12,7 +31,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 		setTimeout(() => {
 			iframe.current.contentWindow.postMessage(code, '*');
 		}, 50);
-	}, [code]);
+	}, [code, rootHtml]);
 
 	return (
 		<div className='preview-wrapper'>
