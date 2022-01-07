b2195de84fe35fdecf06e4115e52dc9f02721521
iivic17 <ilijaivic17@gmail.com>
2022-01-07 14:39:45 +0100

    Fixing resize bugs and implementing correct UX to it

--- src/components/resizable/resizable.tsx
+++ src/components/resizable/resizable.tsx
@@ -1,19 +1,50 @@
 import './resizable.css';
+import { useState, useEffect } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import ResizableProps from './resizable-props';
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 	let resizableBoxProps: ResizableBoxProps;
+	const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+	const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+	const [width, setWidth] = useState(window.innerWidth * 0.75);
+
+	useEffect(() => {
+		let timer: NodeJS.Timeout;
+		const listener = () => {
+			if (timer) {
+				clearTimeout(timer);
+			}
+
+			timer = setTimeout(() => {
+				if (width > window.innerWidth * 0.75) {
+					setWidth(window.innerWidth * 0.75);
+				}
+
+				setInnerHeight(window.innerHeight);
+				setInnerWidth(window.innerWidth);
+			}, 150);
+		};
+
+		window.addEventListener('resize', listener);
+
+		return () => {
+			window.removeEventListener('resize', listener);
+		};
+	}, [width]);
 
 	switch (direction) {
 		case 'horizontal':
 			resizableBoxProps = {
 				className: 'resize-horizontal',
 				height: Infinity,
-				width: window.innerWidth * 0.75,
+				width,
 				resizeHandles: ['e'],
-				maxConstraints: [window.innerWidth * 0.75, Infinity],
-				minConstraints: [window.innerWidth * 0.2, Infinity],
+				maxConstraints: [innerWidth * 0.75, Infinity],
+				minConstraints: [innerWidth * 0.2, Infinity],
+				onResizeStop: (event, data) => {
+					setWidth(data.size.width);
+				},
 			};
 			break;
 		case 'vertical':
@@ -21,7 +52,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 				height: 300,
 				width: Infinity,
 				resizeHandles: ['s'],
-				maxConstraints: [Infinity, window.innerHeight * 0.9],
+				maxConstraints: [Infinity, innerHeight * 0.9],
 				minConstraints: [Infinity, 24],
 			};
 			break;
