4c354892243ee4d75636de50011fd63eb8ce9a48
iivic17 <ilijaivic17@gmail.com>
2022-04-04 15:42:23 +0200

    Insert cell before action reduced

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -42,8 +42,23 @@ const reducer = produce(
 				return;
 
 			case ActionType.INSERT_CELL_BEFORE:
-				// TODO
-				return state;
+				const cell: Cell = {
+					content: '',
+					type: action.payload.type,
+					id: randomId(),
+				};
+
+				state.data[cell.id] = cell;
+
+				const foundIndex = state.order.findIndex(id => id === action.payload.id);
+
+				if (foundIndex < 0) {
+					state.order.push(cell.id);
+				} else {
+					state.order.splice(foundIndex, 0, cell.id);
+				}
+
+				return;
 
 			case ActionType.UPDATE_CELL:
 				const { id, content } = action.payload;
@@ -56,5 +71,9 @@ const reducer = produce(
 		}
 	}
 );
+
+const randomId = () => {
+	return Math.random().toString(36).substring(2, 5);
+};
 
 export default reducer;
