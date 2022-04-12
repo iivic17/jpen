3d57649d6967bfe143398d4e774574d165013ed9
iivic17 <ilijaivic17@gmail.com>
2022-04-13 00:24:49 +0200

    Initial bundles reducer implementation

--- src/state/reducers/bundles-reducer.ts
+++ src/state/reducers/bundles-reducer.ts
@@ -1,1 +1,25 @@
-export {};
+import { Reducer } from 'redux';
+import produce from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface BundlesState {
+    [key: string]: {
+        loading: boolean;
+        code: string;
+        err: string;
+    }
+}
+
+const initialState: BundlesState = {};
+
+const reducer: Reducer<BundlesState, Action> = produce((state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+        case ActionType.BUNDLE_START:
+            return state;
+        case ActionType.BUNDLE_COMPLETE:
+            return state;
+        default:
+            return state;
+    }
+});
