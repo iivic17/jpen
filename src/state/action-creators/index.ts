d8fd54ab25737f43ed5ba9f5db8ebdec21aa9d12
iivic17 <ilijaivic17@gmail.com>
2022-04-13 02:14:12 +0200

    Adding bundle action creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,17 @@
 import { Direction } from './../direction';
 import { ActionType } from '../action-types';
+import { Dispatch } from 'redux';
 import {
+	Action,
 	DeleteCellAction,
 	InsertCellAfterAction,
 	UpdateCellAction,
 	MoveCellAction,
+	BundleStartAction,
+	BundleCompleteAction,
 } from '../actions';
 import { CellType } from '../cell';
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
 	return {
@@ -45,5 +50,26 @@ export const insertCellAfter = (
 			id,
 			type,
 		},
+	};
+};
+
+export const createBundle = (cellId: string, input: string) => {
+	return async (dispatch: Dispatch<Action>) => {
+		dispatch({
+			type: ActionType.BUNDLE_START,
+			payload: {
+				cellId,
+			},
+		});
+
+		const result = await bundle(input);
+
+		dispatch({
+			type: ActionType.BUNDLE_COMPLETE,
+			payload: {
+				cellId,
+				bundle: result,
+			},
+		});
 	};
 };
