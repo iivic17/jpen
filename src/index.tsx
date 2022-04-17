052a9e25dbe027fd0f86a1a6e5429408dbe2e3eb
iivic17 <ilijaivic17@gmail.com>
2022-04-17 16:54:35 +0200

    Bulmaswatch theme switch

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import 'bulmaswatch/united/bulmaswatch.min.css';
+import 'bulmaswatch/minty/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
